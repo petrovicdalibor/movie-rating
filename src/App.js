ec71022c664210979224d93d666032dd8bf4cdc0
Dalibor Petrovic <daliborpskopje@gmail.com>
2023-05-31 16:14:41 +0200

    add header and footer components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 import "./App.scss";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/movie/:imdbID" element={<MovieDetail />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
